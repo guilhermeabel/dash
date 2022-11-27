be062cd9dd47c4ec7e7321fc771d3e3ec6ea8b3d
Abel <guilhermeabel8@gmail.com>
2022-11-27 01:54:35 -0300

    React Entry crud view

---
+++ resources/js/Pages/Entry/Edit.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Inertia } from "@inertiajs/inertia";
+import { InertiaLink, usePage, useForm } from "@inertiajs/inertia-react";
+
+const Edit = () => {
+    const { entry } = usePage().props;
+    const { data, setData, put, errors } = useForm({
+        month: entry.month || "",
+    });
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        put(route("entries.update", entry.id));
+    }
+    function destroy() {
+        if (confirm("Are you sure you want to delete this entry?")) {
+            Inertia.delete(route("entries.destroy", entry.id));
+        }
+    }
+
+    return (
+        <div className="mt-20">
+            <div className="container flex flex-col justify-center mx-auto">
+                <div>
+                    <h1 className="mb-8 text-3xl font-bold">
+                        <InertiaLink
+                            href={route("entries.index")}
+                            className="text-indigo-600 hover:text-indigo-700"
+                        >
+                            Months
+                        </InertiaLink>
+                        <span className="font-medium text-indigo-600"> /</span>
+                        Edit
+                    </h1>
+                </div>
+                <div className="max-w-3xl p-8 bg-white rounded shadow">
+                    <form name="createForm" onSubmit={handleSubmit}>
+                        <div className="flex flex-col">
+                            <div className="mb-4">
+                                <label className="">Month</label>
+                                <input
+                                    type="text"
+                                    className="w-full px-4 py-2"
+                                    label="Month"
+                                    name="month"
+                                    value={data.month}
+                                    onChange={(e) =>
+                                        setData("month", e.target.value)
+                                    }
+                                />
+                                <span className="text-red-600">
+                                    {errors.month}
+                                </span>
+                            </div>
+                        </div>
+                        <div className="flex justify-between">
+                            <button
+                                type="submit"
+                                className="px-4 py-2 text-white bg-green-500 rounded"
+                            >
+                                Update
+                            </button>
+                            <button
+                                onClick={destroy}
+                                tabIndex="-1"
+                                type="button"
+                                className="px-4 py-2 text-white bg-red-500 rounded"
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Edit;

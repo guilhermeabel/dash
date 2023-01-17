aff45005f5c25a11c8ab41dde9a4da1567ba98a3
Abel <guilhermeabel8@gmail.com>
2023-01-16 23:50:27 -0300

    Entry seeder and factory

---
+++ database/seeders/EntrySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Entry;
+
+class EntrySeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        Entry::factory()->count(3)->make();
+    }
+}

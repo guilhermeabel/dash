63628cc67b4e989240d4c90f5ec0ee9c8f9c2b95
Abel <guilhermeabel8@gmail.com>
2023-01-16 23:50:10 -0300

    Middleware auth group

--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,12 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::resource('entries', EntryController::class);
+    Route::resource('items', ItemController::class);
+    Route::resource('imports', ImportController::class);
 
-Route::resource('entries', EntryController::class);
-Route::resource('items', ItemController::class);
-Route::resource('import', ImportController::class);
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});

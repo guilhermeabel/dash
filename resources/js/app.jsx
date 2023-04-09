6b6c53a7760e7a3a61eea543d616a02334ca6578
Guilherme Abel <guilhermeabel8@gmail.com>
2023-04-08 21:20:49 -0300

    add: resume styles

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
 import '../css/custom.scss';
+import '../css/resume.scss';
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';

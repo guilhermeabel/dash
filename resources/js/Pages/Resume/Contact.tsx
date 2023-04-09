51078df660c26d058ae07791bfb1d7d678c2257c
Guilherme Abel <guilhermeabel8@gmail.com>
2023-04-09 10:08:33 -0300

    add: contact info

---
+++ resources/js/Pages/Resume/Contact.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Contact = () => {
+	return <div className="contact">
+		<h3>(work in progress)</h3>
+		<p>linkedin: <a href="https://www.linkedin.com/in/guilherme-abel/" target="_blank">https://www.linkedin.com/in/guilherme-abel/</a></p>
+		<p>github: <a href="https://github.com/guilhermeabel" target="_blank">https://github.com/guilhermeabel</a></p>
+		<p>email: <a href="mailto:guilhermeabel8@gmail.com">guilhermeabel8@gmail.com</a></p>
+	</div>;
+};

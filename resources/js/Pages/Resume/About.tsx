3126b735e3f17570e156061477a0bee3679edd38
Guilherme Abel <guilhermeabel8@gmail.com>
2023-04-09 10:09:51 -0300

    add: updating about details

---
+++ resources/js/Pages/Resume/About.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const About = () => {
+	return <>
+		<h2>hey, I'm Abel</h2>
+		<p className="subtitle">Programmer, tech enthusiast, handsome</p>
+		<p>
+			Hi there! I am a full-stack developer, I'm passionate about creating innovative solutions while following programming best practices. With four years of experience, I have worked on developing web systems for business management, e-commerce, and landing pages.
+		</p>
+		<p>
+			Organizational skills are crucial to me, and I enjoy crafting straightforward yet robust solutions for both my personal and professional projects. I am a fast learner and I am always looking for new challenges.
+		</p>
+		<p>
+			I believe communication is one of the main aspects of modern software development, and I strive to be more transparent and assertive. Collaboration is one of my strengths, but I'm equally comfortable working independently.
+		</p>
+		<p>
+			Besides my professional work, I maintain and update Java and C# projects on GitHub to learn different patterns and engage in coding challenges, which allow me to continuously improve my skills.
+		</p>
+	</>;
+};

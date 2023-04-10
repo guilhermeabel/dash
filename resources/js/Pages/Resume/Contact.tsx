dfbc793202ecdd47ad1d24231c31dede2dae4c56
Guilherme Abel <guilhermeabel8@gmail.com>
2023-04-09 22:23:51 -0300

    feat: contact info update

--- resources/js/Pages/Resume/Contact.tsx
+++ resources/js/Pages/Resume/Contact.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 export const Contact = () => {
 	return <div className="contact">
-		<h3>(work in progress)</h3>
-		<p>linkedin: <a href="https://www.linkedin.com/in/guilherme-abel/" target="_blank">https://www.linkedin.com/in/guilherme-abel/</a></p>
-		<p>github: <a href="https://github.com/guilhermeabel" target="_blank">https://github.com/guilhermeabel</a></p>
-		<p>email: <a href="mailto:guilhermeabel8@gmail.com">guilhermeabel8@gmail.com</a></p>
+		<h3>Feel free to contact me anytime</h3>
+		<p>LinkedIn: <a href="https://www.linkedin.com/in/guilherme-abel/" target="_blank">https://www.linkedin.com/in/guilherme-abel/</a></p>
+		<p>GitHub: <a href="https://github.com/guilhermeabel" target="_blank">https://github.com/guilhermeabel</a></p>
+		<p>Email: <a href="mailto:guilhermeabel8@gmail.com">guilhermeabel8@gmail.com</a></p>
+		<p>Phone: <a href="#">+55 (54) 99203-9734</a></p>
 	</div>;
 };

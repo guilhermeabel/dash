2264070d1dd6341f7777b1c67e1cb7f3191dcab8
Guilherme Abel <guilhermeabel8@gmail.com>
2023-07-13 08:46:55 -0300

    add: route for chat

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
+});
+
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');

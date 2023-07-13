52d6fd02db4b644708c4cf528f154415db8effb4
Guilherme Abel <guilhermeabel8@gmail.com>
2023-07-12 22:10:12 -0300

    refactor: removing deprecated entry/imports logic and components

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
-use App\Http\Controllers\EntryController;
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\ImportController;
 use App\Http\Controllers\ResumeController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-require __DIR__ . '/auth.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -16,27 +9,9 @@ require __DIR__ . '/auth.php';
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
-
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::resource('entries', EntryController::class);
-Route::resource('items', ItemController::class);
-Route::resource('import', ImportController::class);
-Route::resource('resume', ResumeController::class);
 
 Route::get('/', [ResumeController::class, 'index']);

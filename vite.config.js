eeddd77653c44a4de31105c02f7fab32fa6cf873
Guilherme Abel <guilhermeabel8@gmail.com>
2023-07-12 22:23:07 -0300

    refactor: updating config files

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-		checker({ 
-			typescript: true 
+	plugins: [
+		laravel({
+			input: [
+				'resources/js/app.jsx',
+			],
+			refresh: true,
 		}),
-    ],
+		react(),
+		checker({
+			typescript: true
+		}),
+	],
 });

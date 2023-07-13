f145e884253342f93f1f3c0033482129921b2da6
Guilherme Abel <guilhermeabel8@gmail.com>
2023-07-12 22:24:44 -0300

    add: new route

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ResumeController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

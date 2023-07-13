b5d501d4e7bedece4cb3c97244a9af6dd3d8a9cf
Guilherme Abel <guilhermeabel8@gmail.com>
2023-07-12 23:24:59 -0300

    feat: react login ui

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,32 +1,22 @@
 import './bootstrap';
-import '../sass/app.scss';
 import '../css/resume.scss';
-import '../css/loading.scss';
+import '../css/app.css';
 
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/inertia-react';
-import { InertiaProgress } from '@inertiajs/progress';
+import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-
-const globPages = {
-	...import.meta.glob('./Pages/**/*.jsx'),
-	...import.meta.glob('./Pages/**/*.tsx'),
-};
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
 	title: (title) => `${title} - ${appName}`,
-	resolve: (name) => {
-		const page = `./Pages/${name}`;
-		return resolvePageComponent(`${page}.tsx`, globPages) || resolvePageComponent(`${page}.jsx`, globPages);
-	},
+	resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
 	setup({ el, App, props }) {
 		const root = createRoot(el);
 
 		root.render(<App {...props} />);
 	},
+	progress: {
+		color: '#4B5563',
+	},
 });
-
-InertiaProgress.init({ color: '#4B5563' });
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
 	plugins: [
 		laravel({
-			input: [
-				'resources/js/app.jsx',
-			],
+			input: 'resources/js/app.jsx',
 			refresh: true,
 		}),
 		react(),

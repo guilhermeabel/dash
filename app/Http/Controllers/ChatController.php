fe7d338ec540929a0dd84fc2c6c2c05bddb4f405
Guilherme Abel <guilhermeabel8@gmail.com>
2023-07-16 18:11:25 -0300

    refactor: chat logic

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chat;
+use App\Models\Message;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,24 +11,47 @@ class ChatController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Chat/Index');
+        return Inertia::render('Chat/Index')->with("chatId", $this->getUserChatId());
     }
 
-    public function create()
+    public function create(): int
     {
         $chat = new Chat();
-        $chat->userId = auth()->user()->id;
+        $chat->user_id = auth()->user()->id;
         $chat->save();
+
+        return $chat->id;
     }
 
     public function sendRequest(Request $request)
     {
+        // check if chatid from request is from the same user
+        // check if user has exceeded the usage limit
+
+        $request->validate([
+            'newMessage' => 'bail|required|max:5255',
+            'messageHistory' => 'required',
+        ]);
+
         $openAIController = new OpenAIController();
-        $response = $openAIController->sendRequest($request->newMessage, $request->messageHistory);
+        $response = $openAIController->sendRequest($request->newMessage["content"], $request->messageHistory);
 
         $this->persistResults($request, $response);
     }
 
+    public function getChatMessages(int $id)
+    {
+        $messages = Message::where('chat_id', $id)->get();
+        // return only role and content of each message
+        $messages = $messages->map(function ($message) {
+            return [
+                'role' => $message->role,
+                'content' => $message->content,
+            ];
+        });
+
+        return $messages;
+    }
 
 
     private function persistResults($request, $response)
@@ -35,7 +59,7 @@ class ChatController extends Controller
         $messageController = new MessageController();
         $messageController->saveUserMessage(
             $request->chatId,
-            $request->newMessage
+            $request->newMessage["content"]
         );
 
         $messageController->saveResponseMessage(
@@ -47,9 +71,21 @@ class ChatController extends Controller
         $usageController->store(
             $request->chatId,
             $response["model"],
-            $response["usage"]["promptToken"],
-            $response["usage"]["completionToken"],
-            $response["usage"]["totalToken"]
+            $response["usage"]["prompt_tokens"],
+            $response["usage"]["completion_tokens"],
+            $response["usage"]["total_tokens"]
         );
+    }
+
+    private function getUserChatId(): int
+    {
+        $userId = auth()->user()->id;
+        $chat = Chat::where('user_id', $userId)->first();
+
+        if (!$chat) {
+            return $this->create();
+        }
+
+        return $chat->id;
     }
 }

07e07ca5d7a63de80eec5e789f33be732aad9889
Guilherme Abel <guilhermeabel8@gmail.com>
2023-07-16 23:10:11 -0300

    feat: react devtools

--- resources/js/Pages/Chat/Index.tsx
+++ resources/js/Pages/Chat/Index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Chat from "./Chat";
 
 const Index = () => {
@@ -8,6 +9,7 @@ const Index = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<Chat />
+			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	);
 };

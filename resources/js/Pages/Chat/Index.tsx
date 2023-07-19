09db49e768f6c61130a30ac711e17727e8e39cb8
Guilherme Abel <guilhermeabel8@gmail.com>
2023-07-18 23:15:24 -0300

    fix: query provider location

--- resources/js/Pages/Chat/Index.tsx
+++ resources/js/Pages/Chat/Index.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Chat from "./Chat";
 
 const Index = () => {
-	const queryClient = new QueryClient();
 
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Chat />
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		<Chat />
 	);
 };
 
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -8,6 +8,10 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 const globPages = {
@@ -16,14 +20,18 @@ const globPages = {
 };
 
 
-
 createInertiaApp({
 	title: (title) => `${title} - ${appName}`,
 	resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, globPages).catch(() => resolvePageComponent(`./Pages/${name}.tsx`, globPages)),
 	setup({ el, App, props }) {
 		const root = createRoot(el);
 
-		root.render(<App {...props} />);
+		root.render(
+			<QueryClientProvider client={new QueryClient()}>
+				<App {...props} />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
+		);
 	},
 	progress: {
 		color: '#4B5563',

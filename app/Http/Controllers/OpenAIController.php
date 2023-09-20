ce92f28bf13c81d9e71e41a5f3b2d4a3abee55c5
Guilherme Abel <guilhermeabel8@gmail.com>
2023-09-20 15:38:20 -0300

    style: fixer

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -5,24 +5,25 @@ namespace App\Http\Controllers;
 use App\Exceptions\OpenAIRequestException;
 use GuzzleHttp\Client;
 
-class OpenAIController extends Controller
-{
+class OpenAIController extends Controller {
     private const MOCK = true;
+
     /**
      * Send a request to the OpenAI API.
      *
      * @return array
      */
-    public function sendRequest(string $newMessage, array $messageHistory)
-    {
+    public function sendRequest(string $newMessage, array $messageHistory) {
         if (self::MOCK) {
             $response = [];
-            $response['choices'][0]['message']['content'] = "This is an example message: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pellentesque laoreet nisl sit amet lobortis. Donec sed sem et orci tempor molestie. Quisque in mi ut enim tincidunt auctor. Mauris gravida, purus ac condimentum placerat, massa purus vestibulum leo, non condimentum arcu mauris nec augue. ";
-            $response["model"] = 'mock-model-4';
-            $response["usage"]["prompt_tokens"] = 0;
-            $response["usage"]["completion_tokens"] = 0;
-            $response["usage"]["total_tokens"] = 0;
-            sleep(3);
+            $response['choices'][0]['message']['content'] = 'This is an example message: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pellentesque laoreet nisl sit amet lobortis. Donec sed sem et orci tempor molestie. Quisque in mi ut enim tincidunt auctor. Mauris gravida, purus ac condimentum placerat, massa purus vestibulum leo, non condimentum arcu mauris nec augue. ';
+            $response['model'] = 'mock-model-4';
+            $response['usage']['prompt_tokens'] = 0;
+            $response['usage']['completion_tokens'] = 0;
+            $response['usage']['total_tokens'] = 0;
+
+            sleep(1);
+
             return $response;
         }
 
@@ -36,13 +37,13 @@ class OpenAIController extends Controller
                     'messages' => [
                         [
                             'role' => 'system',
-                            'content' => 'You are a helpful assistant'
+                            'content' => 'You are a helpful assistant',
                         ],
                         ...$messageHistory,
                         [
                             'role' => 'user',
-                            'content' => $newMessage
-                        ]
+                            'content' => $newMessage,
+                        ],
                     ],
                     'temperature' => 1.2,
                     'max_tokens' => 1000,
@@ -50,7 +51,6 @@ class OpenAIController extends Controller
             ]);
 
             return json_decode($response->getBody(), true);
-
         } catch (\Exception $e) {
             throw new OpenAIRequestException($e->getMessage());
         }
@@ -58,11 +58,8 @@ class OpenAIController extends Controller
 
     /**
      * Get the headers for the OpenAI API request.
-     *
-     * @return array
      */
-    private function getHeaders(): array
-    {
+    private function getHeaders(): array {
         return [
             'Authorization' => 'Bearer ' . env('OPENAI_API_KEY'),
             'Content-Type' => 'application/json',
